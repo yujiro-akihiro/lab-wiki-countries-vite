294845853d22ea61411270851932fe644bedd5ff
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-06-21 18:10:57 +0200

    #3

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function CountryDetails() {
-    return (
-        <>
-        <h2>Country Details</h2>
-        </>
-    );
+  const { countryId } = useParams();
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+      .then((response) => {
+        setCountry(response.data);
+        // console.log(response.data); データ構造を確認するためにログに出力
+      })
+      .catch((error) => {
+        console.error("Error fetching the country data:", error);
+      });
+  }, [countryId]);
+
+  if (!country) {
+    return <div>Loading...</div>; // Loading OPT
+  }
+
+  return (
+    <div>
+      <h2>Country Details</h2>
+      <h3>{country.name.common}</h3>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area} km²</p>
+      <p>Borders:</p>
+      <ul>
+        {country.borders.map((border) => (
+          <li key={border}>{border}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default CountryDetails;
-
-// CountryDetailsPage component should initially display a headline with the text "Country Details".
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 
@@ -10,7 +11,7 @@ function HomePage() {
       .get("https://ih-countries-api.herokuapp.com/countries")
       .then((response) => {
         setCountries(response.data);
-        console.log(response.data);
+        // console.log(response.data);
       })
       .catch((error) => {
         "Error fetching the countries data", error;
@@ -19,14 +20,22 @@ function HomePage() {
 
   return (
     <>
-    <Navbar />
-    <h1>WikiCountries: Your Guide to the World</h1>
-    <ul>
-        {countries.map(country => (
-            <li key={country.alpha3Code}>{country.name.common}</li>
+      <Navbar />
+      <h1>WikiCountries: Your Guide to the World</h1>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.alpha3Code}>
+            <Link to={`/${country.alpha3Code}`}>
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt={`${country.name.common}`}
+              />
+              {country.name.common}
+            </Link>
+          </li>
         ))}
-    </ul>
-</>
+      </ul>
+    </>
   );
 }
 
